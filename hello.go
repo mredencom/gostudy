5fb47af4b617647ffaf7d757250552da7a4e5a28
jiangtao <mreden@163.com>
2019-01-19 15:52:36 +0800

    'hello'

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	//声明并初始化带缓冲的读取器
+	//准备从标准输入读取输入内容
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("请输入你的名字:")
+	input, err := inputReader.ReadString('\n')
+	if err !=nil {
+		fmt.Printf("发现一个错误: %s\n",err)
+	}else {
+		input = input[:len(input)-1]
+		fmt.Printf("你好: %s\n",input)
+	}
+}
