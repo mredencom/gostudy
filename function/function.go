8be0960fc3365e30b14b52366e3d75c9b8d676cb
jiangtao <mreden@163.com>
2019-02-13 14:14:10 +0800

    readme

--- function/function.go
+++ function/function.go
@@ -6,14 +6,21 @@ import (
 )
 
 func main() {
-	result, err := div(60, 2)
-	if err != nil {
-
-	} else {
-		fmt.Println(result)
-	}
+	//result, err := div(60, 2)
+	//if err != nil {
+	//
+	//} else {
+	//	fmt.Println(result)
+	//}
 	//
 
+}
+
+type myInt int //定义类型
+
+func (i myInt) add(another int) myInt {
+	i = i + myInt(another)
+	return i
 }
 
 //defer 捕获异常
@@ -45,6 +52,7 @@ func div(dividend int, divisor int) (result int, err error) {
 
 //用于定义一个二元操作类型
 type binaryOperation func(operand1 int, operand2 int) (result int, err error)
+
 //用于定义执行二元操作
 func operate(op1 int, op2 int, bop binaryOperation) (result int, err error) {
 	if bop == nil {
