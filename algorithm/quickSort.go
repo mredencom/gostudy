9fad6c59e86f9bf0be21c91e97ae4b63b44b2cc3
jiangtao <mreden@163.com>
2019-03-06 08:55:49 +0800

    code

---
+++ algorithm/quickSort.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/**
+快速排序
+ */
+
+func QuickSort(nums []int, start, end int) {
+	//fmt.Print(nums)
+	//os.Exit(0)
+	if start < end {
+		i, j := start, end
+		//fmt.Print((start+end)/2)
+		mid := nums[(start+end)/2]
+		for i < + j {
+			for nums[i] < mid {
+				i++
+			}
+			for nums[j] < mid {
+				j--
+			}
+			if i <= j {
+				nums[i], nums[j] = nums[j], nums[i]
+				i++
+				j--
+			}
+		}
+		if start < j {
+			QuickSort(nums, start, j)
+		}
+		if end > i {
+			QuickSort(nums, i, end)
+		}
+	}
+	//fmt.Print(nums)
+}
+func main() {
+	var nums = []int{2, 34, 57, 12, 4, 64, 12431}
+	QuickSort(nums, 2, len(nums)-1)
+	fmt.Println(nums)
+}
--- https/https.go
+++ https/https.go
@@ -1,14 +1,1 @@
-package main
-
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(writer http.ResponseWriter, request *http.Request) {
-	_, _ = fmt.Fprintf(writer, "hello world %s", request.URL.Path[1:])
-}
-func main() {
-	http.HandleFunc("/", handler)
-	_ = http.ListenAndServe(":8089", nil)
-}
+package mainimport (	"fmt"	"net/http")func handler(writer http.ResponseWriter, request *http.Request) {	_, _ = fmt.Fprintf(writer, "hello world %s", request.URL.Path[1:])}func main() {	http.HandleFunc("/", handler)	_ = http.ListenAndServe(":8089", nil)}
