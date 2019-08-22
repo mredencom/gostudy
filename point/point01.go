d6d0e711cfe958cc1d38d2db1a7d7c17620a20bc
MrEden <mreden@163.com>
2019-08-22 10:12:08 +0800

    point

---
+++ point/point01.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var p = f()
+
+func main() {
+	//fmt.Println(f() == f()) //false
+	v := 1
+	fmt.Println(incr(&v))
+	fmt.Println(incr(&v))
+}
+
+func f() *int {
+	v := 1
+	return &v
+}
+
+func incr(c *int) int {
+	*c++ //非常注意：这里的增加c指向的值，不会改变c指针
+	return *c
+}
